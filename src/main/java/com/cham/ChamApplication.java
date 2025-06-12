7ae652c3958a51ef7cc2d965746260301ca63050
PeachCoolpis <rlglxh@naver.com>
2025-06-13 01:51:45 +0900

    프로젝트 초기세팅

---
+++ src/main/java/com/cham/ChamApplication.java
@@ -0,0 +1,13 @@
+package com.cham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(ChamApplication.class, args);
+    }
+    
+}
