9cd51295624af36170bccd87bfac6d0580f2a466
배성환 <baeseongwhan@ulim.co>
2025-06-13 09:57:12 +0900

    백엔드 공통 소스 세팅

--- src/main/java/com/cham/ChamApplication.java
+++ src/main/java/com/cham/ChamApplication.java
@@ -1,7 +1,10 @@
 package com.cham;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChamApplication {
@@ -9,5 +12,8 @@ public class ChamApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChamApplication.class, args);
     }
-    
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
 }
