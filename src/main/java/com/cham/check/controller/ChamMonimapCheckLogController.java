482fa9a093955f3fc1b960eafedb6b388805169c
배성환 <baeseongwhan@ulim.co>
2025-09-23 13:58:24 +0900

    의심되여 가봤어요 컨트롤러 추가

--- src/main/java/com/cham/check/controller/ChamMonimapCheckLogController.java
+++ src/main/java/com/cham/check/controller/ChamMonimapCheckLogController.java
@@ -1,8 +1,12 @@
 package com.cham.check.controller;
 
 
+import com.cham.check.dto.CheckLogGetResponse;
+import com.cham.check.service.ChamMonimapCheckLogService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ChamMonimapCheckLogController {
 
-
+    private final ChamMonimapCheckLogService chamMonimapCheckLogService;
+    
+    @GetMapping("/check")
+    public CheckLogGetResponse getCheckLog(@RequestParam(name = "addrId") Long addrId) {
+        return chamMonimapCheckLogService.findByCheckAggregation(addrId);
+    }
 }
