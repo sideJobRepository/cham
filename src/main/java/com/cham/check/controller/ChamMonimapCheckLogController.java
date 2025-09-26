a93f4b58f9d7bad7ac7ef06a02eff0a2cedf746a
배성환 <baeseongwhan@ulim.co>
2025-09-26 14:55:16 +0900

    멤버 ID 안받게 수정

--- src/main/java/com/cham/check/controller/ChamMonimapCheckLogController.java
+++ src/main/java/com/cham/check/controller/ChamMonimapCheckLogController.java
@@ -7,6 +7,11 @@ import com.cham.check.dto.response.CheckLogGetResponse;
 import com.cham.check.service.ChamMonimapCheckLogService;
 import com.cham.dto.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,17 +20,24 @@ import org.springframework.web.bind.annotation.*;
 public class ChamMonimapCheckLogController {
 
     private final ChamMonimapCheckLogService chamMonimapCheckLogService;
-    
+
     @GetMapping("/check")
-    public CheckLogGetResponse getCheckLog(@RequestParam(name = "addrId") Long addrId , @RequestParam(name = "memberId",required = false) Long memberId) {
-        return chamMonimapCheckLogService.findByCheckAggregation(addrId, memberId);
+    public CheckLogGetResponse getCheckLog(@RequestParam(name = "addrId") Long addrId) {
+        Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            return chamMonimapCheckLogService.findByCheckAggregation(addrId, null);
+        }
+        JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) authentication;
+        Jwt token = jwtAuthenticationToken.getToken();
+        Long id = token.getClaim("id");
+        return chamMonimapCheckLogService.findByCheckAggregation(addrId, id);
     }
-    
+
     @PostMapping("/check-visited")
     public ApiResponse createVisitedLog(@RequestBody CheckLogVisitedPostRequest request) {
         return chamMonimapCheckLogService.createCheckLogVisit(request);
     }
-    
+
     @PostMapping("/check-suspicious")
     public ApiResponse createSuspicious(@RequestBody CheckLogSuspiciousPostRequest request) {
         return chamMonimapCheckLogService.createCheckLogSuspicious(request);
