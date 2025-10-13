7a590b1c59e3b563000b828dcb6ac1066dc0633f
배성환 <baeseongwhan@ulim.co>
2025-10-13 14:38:15 +0900

    불필요 메서드 삭제

--- src/main/java/com/cham/caruse/service/impl/ChamMonimapCardUseServiceImpl.java
+++ src/main/java/com/cham/caruse/service/impl/ChamMonimapCardUseServiceImpl.java
@@ -636,22 +636,4 @@ public class ChamMonimapCardUseServiceImpl implements ChamMonimapCardUseService 
                 .depth2(depth2)
                 .build();
     }
-    private String normalizeRegionName(String name) {
-        if (name == null) return null;
-        return name
-                .replace("충청북도", "충북")
-                .replace("충청남도", "충남")
-                .replace("경상북도", "경북")
-                .replace("경상남도", "경남")
-                .replace("전라북도", "전북")
-                .replace("전라남도", "전남")
-                .replace("제주특별자치도", "제주")
-                .replace("서울특별시", "서울")
-                .replace("대전광역시", "대전")
-                .replace("부산광역시", "부산")
-                .replace("인천광역시", "인천");
-    }
-    
-    
-    
 }
