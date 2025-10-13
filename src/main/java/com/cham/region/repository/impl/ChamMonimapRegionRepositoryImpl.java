20924118a6e0db8c477e938806508331f5e91063
배성환 <baeseongwhan@ulim.co>
2025-10-13 14:09:01 +0900

    안쓰는 쿼리 삭제

--- src/main/java/com/cham/region/repository/impl/ChamMonimapRegionRepositoryImpl.java
+++ src/main/java/com/cham/region/repository/impl/ChamMonimapRegionRepositoryImpl.java
@@ -13,47 +13,6 @@ public class ChamMonimapRegionRepositoryImpl implements ChamMonimapRegionQueryRe
 
     private final JPAQueryFactory queryFactory;
     
-    @Override
-    public ChamMonimapRegion findByCity(String region1depthName) {
-        return queryFactory
-                .selectFrom(chamMonimapRegion)
-                .where(chamMonimapRegion.chamMonimapRegionName.eq(region1depthName),chamMonimapRegion.chamMonimapRegionDepth.eq(0))
-                .fetchFirst();
-    }
-    
-    @Override
-    public ChamMonimapRegion findByGu(String region1depthName, String region2depthName) {
-        QChamMonimapRegion gu    = new QChamMonimapRegion("gu");
-        QChamMonimapRegion city  = new QChamMonimapRegion("city");
-        return queryFactory
-                .selectFrom(gu)
-                .join(gu.parent,  city)
-                .where(
-                        city.chamMonimapRegionName.eq(region1depthName),
-                        gu.chamMonimapRegionName.eq(region2depthName),
-                        gu.chamMonimapRegionDepth.eq(1)
-                )
-                .fetchFirst();
-    }
-    
-    @Override
-    public ChamMonimapRegion findByDong(String region1depthName, String region2depthName, String region3depthName) {
-        QChamMonimapRegion dong  = new QChamMonimapRegion("dong");
-        QChamMonimapRegion gu    = new QChamMonimapRegion("gu");
-        QChamMonimapRegion city  = new QChamMonimapRegion("city");
-        
-        return queryFactory
-                .selectFrom(dong)
-                .join(dong.parent, gu)
-                .join(gu.parent,  city)
-                .where(
-                        city.chamMonimapRegionName.eq(region1depthName),
-                        gu.chamMonimapRegionName.eq(region2depthName),
-                        dong.chamMonimapRegionName.eq(region3depthName),
-                        dong.chamMonimapRegionDepth.eq(2)
-                )
-                .fetchFirst();
-    }
     
     @Override
     public ChamMonimapRegion findByNameAndDepth(String name, int depth) {
--- src/main/java/com/cham/region/repository/query/ChamMonimapRegionQueryRepository.java
+++ src/main/java/com/cham/region/repository/query/ChamMonimapRegionQueryRepository.java
@@ -5,12 +5,6 @@ import com.cham.region.entity.ChamMonimapRegion;
 public interface ChamMonimapRegionQueryRepository {
     
     
-    ChamMonimapRegion findByCity(String region1depthName);
-    
-    ChamMonimapRegion findByGu(String region1depthName,String region2depthName);
-    
-    ChamMonimapRegion findByDong(String region1depthName,String region2depthName,String region3depthName);
-    
     ChamMonimapRegion findByNameAndDepth(String name, int depth);
     
     
