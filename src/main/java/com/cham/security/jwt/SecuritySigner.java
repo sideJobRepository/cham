4d00772d4c4186b85857191751d1fda956f39713
배성환 <baeseongwhan@ulim.co>
2025-10-20 12:42:11 +0900

    토큰 유효시간 1시간으로 임시 변경

--- src/main/java/com/cham/security/jwt/SecuritySigner.java
+++ src/main/java/com/cham/security/jwt/SecuritySigner.java
@@ -38,8 +38,8 @@ public abstract class SecuritySigner {
                 .claim("socialId", user.getChamMonimapMemberSubId())
                 .claim("expirationTime",date)
                 .claim("roles", roleList)
-               // .expirationTime(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1시간
-                .expirationTime(new Date(System.currentTimeMillis() + 1000 * 60 * 5)) // 5분
+                .expirationTime(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1시간
+                // .expirationTime(new Date(System.currentTimeMillis() + 1000 * 60 * 5)) // 5분
               //  .expirationTime(new Date(System.currentTimeMillis() +10))
                 .build();
         
