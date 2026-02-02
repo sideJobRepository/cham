b825a7b0239c2f90000f956478be797bfcb8ecfc
parkjs <park74441@naver.com>
2026-02-02 18:54:26 +0900

    fix. 메타 변경

--- cham-next-front/src/app/layout.tsx
+++ cham-next-front/src/app/layout.tsx
@@ -7,11 +7,12 @@ import { fetchMenuDataOrThrow } from '@/lib/server/menu';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://cham-monimap.com'),
-  title: '대전참여자치시민연대',
+  title: '충남대전통합 특별법(안) 공개 시민의 의견을 말해주세요.',
   description: '대전참여자치시민연대 법령 의견수렴 서비스',
   openGraph: {
-    title: '대전참여자치시민연대',
-    description: '대전참여자치시민연대 법령 의견수렴 서비스',
+    title: '충남대전통합 특별법(안) 공개 시민의 의견을 말해주세요.',
+    description:
+      '공개된 여당의 광역행정통합 법안을 직접 확인하고, 각 조항별로 의견을 더해주세요. 여러분이 남긴 의견을 모아 전달하겠습니다.',
     url: 'https://cham-monimap.com/feedback',
     images: [
       {
@@ -24,8 +25,9 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    title: '대전참여자치시민연대',
-    description: '대전참여자치시민연대 법령 의견수렴 서비스',
+    title: '충남대전통합 특별법(안) 공개 시민의 의견을 말해주세요.',
+    description:
+      '공개된 여당의 광역행정통합 법안을 직접 확인하고, 각 조항별로 의견을 더해주세요. 여러분이 남긴 의견을 모아 전달하겠습니다.',
     images: ['https://cham-monimap.com/feedback/og.png'],
   },
 };
